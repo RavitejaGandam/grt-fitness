cb19f182d1c75cc9205874e6b5dc6561bbad6d66
Raviteja Gandam <gandamraviteja@gmail.com>
2024-06-19 13:35:46 +0530

    Partial dev

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Generator from "./components/Generator";
+import Hero from "./components/Hero";
+import Workout from "./components/Workout";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base'>
+      <Hero />
+      <Generator />
+      <Workout />
+    </main>
   );
 }
 
