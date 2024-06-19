65faadbcef0d5c40d0df12922ed7ad2d1d562424
Raviteja Gandam <gandamraviteja@gmail.com>
2024-06-19 13:49:31 +0530

    added a component

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import SectionWrapper from "./SectionWrapper";
 
 function Generator() {
-  return <div>Generator</div>;
+  return (
+    <SectionWrapper
+      className="min-h-screen"
+      header={"generate your workout"}
+      title={["It's", "Huge", "o'clock"]}
+    >
+      RaviTeja
+    </SectionWrapper>
+  );
 }
 
 export default Generator;
